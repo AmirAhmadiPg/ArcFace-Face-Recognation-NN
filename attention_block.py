7379ea5eb3d14fc6367a2e33ea7d7528e708b691
AmirAhmadiPg <amirahmadi.pg@gmail.com>
2021-08-01 15:23:52 +0430

    first commit

--- attention_block.py
+++ attention_block.py
@@ -1,19 +1,14 @@
 from tensorflow.keras import layers
 
-def OneAttentionBlock(PLayer, n_filters):
+def Network_block(PLayer, n_filters):
 
-    key = layers.Conv2D(n_filters, (1, 1), activation=None, use_bias=False)(PLayer)
+    Conv0 = layers.Conv2D(n_filters, (3, 3), activation=None, use_bias=False)(PLayer)
 
-    query = layers.Conv2D(n_filters, (1, 1), activation=None, use_bias=False)(PLayer)
+    Conv1 = layers.Conv2D(n_filters, (3, 3), activation=None, use_bias=False)(Conv0)
 
-    values = layers.Conv2D(1, (1, 1), activation=None, use_bias=False)(PLayer)
+    Conv2 = layers.Conv2D(n_filters, (3, 3), activation=None, use_bias=False)(Conv1)
 
-    attention_kernel = layers.Multiply()([key, query])
 
-    attention_kernel_normilized = layers.Softmax()(attention_kernel)
-
-    attention_output = layers.Multiply()([attention_kernel_normilized, values])
-
-    return attention_output
+    return Conv2
 
 
